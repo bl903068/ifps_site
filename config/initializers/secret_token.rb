8179eee81f5478b898c28e959808a886b0cbf59c
bl903068 <brouchetlionel@gmail.com>
2012-12-26 11:07:32 +0100

    initialisation projet

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IfpsSite::Application.config.secret_token = '46d8c73f27cffe3568a5b2139b21df58d7d8923175a89828d3238e08d869280ab6a8f3f2e0c6183f28d43877be7bc5302c59adb0c8edfc8e0d02a4c66127a7f2'
