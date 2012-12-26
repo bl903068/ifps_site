7581484367f4ff4cb210a760e397256620c420f8
bl903068 <brouchetlionel@gmail.com>
2012-12-26 19:22:58 +0100

    Mise en place de bootstrap/embelissement des test

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,19 +1,14 @@
 require 'spec_helper'
 
 describe "Static pages" do
-	describe "Home page" do
-    	it "should have the h1 'Site IFPS'" do
-      		visit '/static_pages/home'
-      		page.should have_selector('h1', 'Site IFPS')
-    	end
 
-    	it "should have the base title" do
-      		visit '/static_pages/home'
-      		page.should have_selector('title', "Site IFPS")
-    	end
-    	it "should not have a custom page title" do
-    		visit '/static_pages/home'
-    		page.should_not have_selector('title', '| Home')
-    	end
+	subject { page }
+
+	describe "Home page" do
+		before { visit root_path } 
+
+    	it { should have_selector('h1', text: 'Acceuil') }
+    	it { should have_selector('title', "Site IFPS") }
+    	it { should_not have_selector('title', text: '| Home') }
     end
 end
